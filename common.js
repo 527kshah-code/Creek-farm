47de03a729a423d161ef17887d5bb52a4727e46e
527kshah <527kshah@frhsd.com>
2026-06-15 13:04:21 -0400

    update

--- common.js
+++ common.js
@@ -198,17 +198,17 @@ function displayStore(list) {
         card.classList.add("col");
 
         card.innerHTML = `
-            <div class="card h-100">
+            <div class="card h-100 product-card">
                 <img src="${item.image}" class="card-img-top" alt="${item.name}">
                 
-                <div class="card-body">
-                    <h5 class="card-title">${item.name}</h5>
+                <div class="card-content">
+                    <h3 class="card-title">${item.name}</h3>
 
                     <p class="card-text">
                         ${item.description}
                     </p>
 
-                    <p class="card-text">
+                    <p class="price">
                         <strong>Price: ${item.price}</strong>
                     </p>
                 </div>
